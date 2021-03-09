c2d4b38cf69895911ed9025511553b403b973811
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2021-03-09 11:12:51 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+*head   <link rel="stylesheet" href="style.css">
+<div class="form-box">
+        <h1>Login Here</h1>
+        <div class="input-box">
+            <input type="username" placeholder="User Name">
+        </div>
+        <div class="input-box">
+            <input type="password" placeholder="Password">
+        </div>
+    </div>
