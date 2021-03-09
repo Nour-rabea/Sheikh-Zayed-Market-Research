5534b1d1759ca4d79f3a1d0c11c522f2d87bd1ce
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2021-03-09 11:28:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-*head   <link rel="stylesheet" href="style.css">
-<div class="form-box">
-        <h1>Login Here</h1>
-        <div class="input-box">
-            <input type="username" placeholder="User Name">
-        </div>
-        <div class="input-box">
-            <input type="password" placeholder="Password">
-        </div>
-    </div>
+head
+        <link rel="stylesheet" href="style.css">
+                <div class="form-box">
+                        <h1>Login Here</h1>
+<div class="input-box">
+        <input type="username" placeholder="User Name"> </div>
+<div class="input-box">
+        <input type="password" placeholder="Password"> </div> </div>
